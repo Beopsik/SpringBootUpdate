31899890c1349f9dc761aee6d28ce9c917795e73
BeobSik <tjswl756@naver.com>
2021-09-14 15:40:59 +0900

    feat: TaskSchedulingAutoConfiguration(@EnableScheduling)

--- src/main/java/com/example/springbootupdate/BookService.java
+++ src/main/java/com/example/springbootupdate/BookService.java
@@ -1,6 +1,7 @@
 package com.example.springbootupdate;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +10,10 @@ public class BookService {
     @Async
     public void hello(){
         System.out.println("hello "+Thread.currentThread().getName());
+    }
+
+    @Scheduled(fixedDelay = 1000*2)
+    public void hi(){
+        System.out.println("hi "+Thread.currentThread().getName());
     }
 }
